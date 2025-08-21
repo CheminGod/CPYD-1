db1701bf7fa87db239fac6623c8eca2db76c834f
Guillermo Ortiz <131318874+CheminGod@users.noreply.github.com>
2025-08-20 20:07:42 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,129 @@
+#include <memory>
+#include <string>
 #include <iostream>
 #include <thread>
-#include <cstdlib>
-#include <ctime>
+//#include <print>
 #include <mutex>
-#include <random>
+#include <condition_variable>
 
-int results[10]; 
-std::mutex mtx;  
+class Fork {
+private:
+    // bool occupied;
+    std::mutex mutex;
+    std::condition_variable taken;
 
+public:
+    bool occupied = false;
 
-void generate(int id) {
-    
-    std::random_device rd;  
-    std::mt19937 gen(rd()); 
-    std::uniform_int_distribution<> dis(0, 1000); 
+    void pickup(std::string_view philosopherName, std::string_view hand) {
+        {
+        std::unique_lock lock(mutex);
+        taken.wait(lock, [&] {
+            std::cout << philosopherName << " is waiting for " << hand << " hand fork" << std::endl;
+            return !occupied;
+        });
+        std::cout << philosopherName << " is taking " << hand << " hand fork" << std::endl;
 
-    int sum = 0;
-    for (int i = 0; i < 100; i++) {
-        int num = dis(gen); 
-        sum += num;
-    }
-    results[id] = sum;
-
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        std::cout << "Thread " << id + 1 << " obtuvo suma = " << sum << std::endl;
-    }
-}
-
-int main() {
-    srand(time(0)); 
-    std::thread threads[10];
-
-    // lanzar los 10 threads
-    for (int i = 0; i < 10; i++) {
-        threads[i] = std::thread(generate, i);
-    }
-
-    // esperar que terminen
-    for (int i = 0; i < 10; i++) {
-        threads[i].join();
-    }
-
-    // encontrar el ganador
-    int maxScore = results[0];
-    int bestThread = 0;
-    for (int i = 1; i < 10; i++) {
-        if (results[i] > maxScore) {
-            maxScore = results[i];
-            bestThread = i + 1;
+        occupied = true;
         }
     }
 
-    std::cout << "\nEl thread con la puntuacion mas alta fue el Thread "
-              << bestThread << " con suma = " << maxScore << std::endl;
+    void putDown(std::string_view philosopherName, std::string_view hand) {
+        std::cout << philosopherName << " is releasing " << hand << " hand fork" << std::endl;
+        occupied = false;
+        taken.notify_all();
+    }
+
+    void printState(const std::string &name, const std::string &hand) const {
+        std::cout << "{} is {} and {}", name, hand, occupied ? "occupied" : "available";
+
+    }
+};
+
+class Philosopher {
+private:
+    std::string name;
+    std::shared_ptr<Fork> leftFork;
+    std::shared_ptr<Fork> rightFork;
+
+public:
+    std::shared_ptr <Fork> RightFork() const {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> &RightFork() {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> LeftFork() const {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> &LeftFork() {
+        return rightFork;
+    }
+
+
+     void eat() {
+        while(true) {
+        leftFork->pickup(name, "left");
+        if (rightFork->occupied) {
+            leftFork->putDown(name, "left");
+            continue;
+        }
+        rightFork->pickup(name, "right");
+
+        // Simulating the Philosopher eating
+        std::cout << name << " started eating" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << name << " is done eating" << std::endl;
+
+        leftFork->putDown(name, "left");
+
+        rightFork->putDown(name, "right");
+        }
+    }
+
+    Philosopher(std::string name ) : name(name){}
+
+}; 
+
+int main() {
+    std::shared_ptr<Fork> fork1 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork2 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork3 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork4 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork5 = std::make_shared<Fork>();
+
+    Philosopher socrates("Socrates");
+    socrates.LeftFork() = fork1;
+    socrates.RightFork() = fork2;
+    Philosopher descartes("Descartes");
+    socrates.LeftFork() = fork2;
+    socrates.RightFork() = fork1;
+    // socrates.eat();
+    Philosopher plato("Plato");
+    plato.LeftFork() = fork3;
+    plato.RightFork() = fork4;
+    Philosopher aristotle("Aristotle");
+    aristotle.LeftFork() = fork4;
+    aristotle.RightFork() = fork5;
+    Philosopher kant("Kant");
+    kant.LeftFork() = fork5;    
+    kant.RightFork() = fork1;
+
+    std::thread threadDescartes([&](){ descartes.eat(); });
+    std::thread threadSocrates([&](){ socrates.eat(); });
+    std::thread threadPlato([&](){ plato.eat(); });
+    std::thread threadAristotle([&](){ aristotle.eat(); });
+    std::thread threadKant([&](){ kant.eat(); });
+
+    threadSocrates.join();
+    threadDescartes.join();
+    threadPlato.join();
+    threadAristotle.join();
+    threadKant.join();
+
 
     return 0;
 }
