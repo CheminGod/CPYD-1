1342d11039bcb04056025bb64b583dfbdfc36eff
CheminGod <guillermortiz261@gmail.com>
2025-08-28 02:33:23 +0000

    Actvidad 1 y 2 agregadas

--- codes/act1/main.cpp
+++ codes/act1/main.cpp
@@ -1,3 +1,129 @@
-//
-// Created by developer on 8/28/25.
-//
+#include <memory>
+#include <string>
+#include <iostream>
+#include <thread>
+//#include <print>
+#include <mutex>
+#include <condition_variable>
+
+class Fork {
+private:
+    // bool occupied;
+    std::mutex mutex;
+    std::condition_variable taken;
+
+public:
+    bool occupied = false;
+
+    void pickup(std::string_view philosopherName, std::string_view hand) {
+        {
+        std::unique_lock lock(mutex);
+        taken.wait(lock, [&] {
+            std::cout << philosopherName << " is waiting for " << hand << " hand fork" << std::endl;
+            return !occupied;
+        });
+        std::cout << philosopherName << " is taking " << hand << " hand fork" << std::endl;
+
+        occupied = true;
+        }
+    }
+
+    void putDown(std::string_view philosopherName, std::string_view hand) {
+        std::cout << philosopherName << " is releasing " << hand << " hand fork" << std::endl;
+        occupied = false;
+        taken.notify_all();
+    }
+
+    void printState(const std::string &name, const std::string &hand) const {
+        std::cout << "{} is {} and {}", name, hand, occupied ? "occupied" : "available";
+
+    }
+};
+
+class Philosopher {
+private:
+    std::string name;
+    std::shared_ptr<Fork> leftFork;
+    std::shared_ptr<Fork> rightFork;
+
+public:
+    std::shared_ptr <Fork> RightFork() const {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> &RightFork() {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> LeftFork() const {
+        return rightFork;
+    }
+
+    std::shared_ptr <Fork> &LeftFork() {
+        return rightFork;
+    }
+
+
+     void eat() {
+        while(true) {
+        leftFork->pickup(name, "left");
+        if (rightFork->occupied) {
+            leftFork->putDown(name, "left");
+            continue;
+        }
+        rightFork->pickup(name, "right");
+
+        // Simulating the Philosopher eating
+        std::cout << name << " started eating" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << name << " is done eating" << std::endl;
+
+        leftFork->putDown(name, "left");
+
+        rightFork->putDown(name, "right");
+        }
+    }
+
+    Philosopher(std::string name ) : name(name){}
+
+};
+
+int main() {
+    std::shared_ptr<Fork> fork1 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork2 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork3 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork4 = std::make_shared<Fork>();
+    std::shared_ptr<Fork> fork5 = std::make_shared<Fork>();
+
+    Philosopher socrates("Socrates");
+    socrates.LeftFork() = fork1;
+    socrates.RightFork() = fork2;
+    Philosopher descartes("Descartes");
+    socrates.LeftFork() = fork2;
+    socrates.RightFork() = fork1;
+    // socrates.eat();
+    Philosopher plato("Plato");
+    plato.LeftFork() = fork3;
+    plato.RightFork() = fork4;
+    Philosopher aristotle("Aristotle");
+    aristotle.LeftFork() = fork4;
+    aristotle.RightFork() = fork5;
+    Philosopher kant("Kant");
+    kant.LeftFork() = fork5;
+    kant.RightFork() = fork1;
+
+    std::thread threadDescartes([&](){ descartes.eat(); });
+    std::thread threadSocrates([&](){ socrates.eat(); });
+    std::thread threadPlato([&](){ plato.eat(); });
+    std::thread threadAristotle([&](){ aristotle.eat(); });
+    std::thread threadKant([&](){ kant.eat(); });
+
+    threadSocrates.join();
+    threadDescartes.join();
+    threadPlato.join();
+    threadAristotle.join();
+    threadKant.join();
+
+
+    return 0;
+}
--- codes/act2/main.cpp
+++ codes/act2/main.cpp
@@ -1,3 +1,85 @@
-//
-// Created by developer on 8/28/25.
-//
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>  
+#include <cstdlib>
+#include <ctime>
+#include <mutex>
+#include <random>
+#include <algorithm>
+
+std::mutex mtx;
+
+class ThreadSum {
+private:
+    int id;
+    int sum;
+
+public:
+    ThreadSum(int thread_id) : id(thread_id), sum(0) {}
+
+    int getSum() const {
+        return sum;
+    }
+
+    int getId() const {
+        return id;
+    }
+
+    void generate() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 1000);
+
+        for (int i = 0; i < 100; i++) {
+            sum += dis(gen);
+        }
+
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "El Thread " << id + 1 << " obtuvo una suma = " << sum << std::endl;
+    }
+};
+
+int main() {
+   
+    std::vector<std::unique_ptr<ThreadSum>> thread_sums;
+    std::vector<std::thread> threads;
+
+    // lanzar los 10 threads
+
+    for (int i = 0; i < 10; ++i) {
+        thread_sums.emplace_back(std::make_unique<ThreadSum>(i));
+        threads.emplace_back(&ThreadSum::generate, thread_sums[i].get());
+    }
+
+    // esperar que terminen
+
+    for (std::thread &t : threads) {
+        t.join();
+    }
+
+    // encontrar el ganador
+
+    /*int maxScore = 0;
+    int bestThreadId = -1;
+
+    for (const auto& thread_sum : thread_sums) {
+        if (thread_sum->getSum() > maxScore) {
+            maxScore = thread_sum->getSum();
+            bestThreadId = thread_sum->getId();
+        }
+    }*/
+
+    std::sort(thread_sums.begin(), thread_sums.end(),
+            [](const std::unique_ptr<ThreadSum>& a, const std::unique_ptr<ThreadSum>& b) {
+                return a->getSum() < b->getSum();
+            });
+
+    
+    const auto& ganador = thread_sums.back();
+
+    std::cout << "\nEl thread con la puntuacion mas alta fue el Thread "
+            << ganador->getId() + 1 << " con una suma = " << ganador->getSum() << std::endl;
+
+    return 0;
+}
